edcabdec21ae9562de19df5d25eaea7b93612410
yoyoyun <lxy691219491@bupt.edu.cn>
2017-05-18 09:02:45 +0800

    Update worktime.py

--- worktime.py
+++ worktime.py
@@ -2,7 +2,6 @@ import xlwt
 import xlrd
 from xlutils.copy import copy
 
-
 arriveTime = raw_input("ArriveTime: ") 
 leaveTime = raw_input("LeaveTime: ")   
 
@@ -10,19 +9,16 @@ aT = arriveTime.split(':')
 lT = leaveTime.split(':')
 
 workHour = '%.2f' % (int(lT[0])-1 - int(aT[0]) + ( float(lT[1])+60 - float(aT[1]) ) / 60  -1)
-#print workHour
 
 data = xlrd.open_workbook('WorkTime.xls')
 table = data.sheet_by_name(u'My WorkTime')
-#print table.nrows
+
 row = table.nrows
 newdata = copy(data)
 newtable = newdata.get_sheet(0)
 newtable.write(row,0,workHour)
 newdata.save('WorkTime.xls')
 
-#f = xlwt.Workbook()
-#sheet1 = f.add_sheet(r'sheet1',cell_overwrite_ok=True)
-#sheet1.write(0,0,value)
+
 
 
